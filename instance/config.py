644b241bac983ef06784a86f55994feb013a5ca9
Espoir Murhabazi <espoir.mur@gmail.com>
2018-07-31 19:25:39 +0200

    removing staging and testing config

--- instance/config.py
+++ instance/config.py
@@ -15,28 +15,12 @@ class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
 
-
-class TestingConfig(Config):
-    """Configurations for Testing, with a separate test database."""
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/flask_api_test.db'
-    DEBUG = True
-
-
-class StagingConfig(Config):
-    """Configurations for Staging."""
-    DEBUG = True
-
-
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
 
-
 app_config = {
     'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'staging': StagingConfig,
     'production': ProductionConfig,
 }
