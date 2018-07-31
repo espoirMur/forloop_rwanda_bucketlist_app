422a8199ea8e4e1d9c98d32b7ebeb18b33724ced
Espoir Murhabazi <espoir.mur@gmail.com>
2018-07-31 19:27:02 +0200

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -6,7 +6,6 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/flask_api.db'
     # for windows sqlite:///c:/absolute/path/to/database
 
